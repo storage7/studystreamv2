1f9b69305b15be394d8bc85be3d719587fbbe14b
storage7 <storage2.7777@gmail.com>
2026-07-30 22:29:56 +0530

    Update route.ts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -46,7 +46,15 @@ export async function POST(request: NextRequest) {
         role: user.role,
       },
     });
-  } catch {
-    return NextResponse.json({ redirect: true }, { status: 401 });
+  } catch (error) {
+    console.error("LOGIN ERROR:", error);
+  
+    return NextResponse.json(
+      {
+        success: false,
+        error: String(error),
+      },
+      { status: 500 }
+    );
   }
 }
